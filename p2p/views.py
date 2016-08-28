9373b712334cacecdc7daed90a3d5fa481ad2c12
Jake <jake.poirier55@gmail.com>
2016-08-28 13:58:52 -0600

    Add parent to each file

--- p2p/views.py
+++ p2p/views.py
@@ -9,53 +9,9 @@ import logging
 
 
 def index(request):
-    neighbors = Neighbors.objects.all()
-    filename = request.GET.get('filename')
-    if filename is None:
-        filename = ''
-    aggregate_list = {}
-    for neighbor in neighbors:
-        response = requests.get('http://{0}:{1}/api/v1/filelist'.format(neighbor.ip_address, neighbor.port))
-        aggregate_list[neighbor.hostname] = response.text
-
-    return render(request, 'index.html', {'files': aggregate_list,
-                                          'filename': filename})
 
 
-def filelist_api(request):
-    host, port = request.META['HTTP_HOST'].split(':')
-    neighbors = Neighbors.objects.all()
-    print 'NEIGHBORS', neighbors
-    filename = request.GET.get('filename')
-    hop_number = int(request.GET.get('hop'))
-    hop_number += 1
-    filelist = File.objects.all()
-    json_response = []
-    for file in filelist:
-        json_response.append({'name': file.name,
-                              'location': file.location,
-                              'category': file.category,
-                              'host': host,
-                              'port': port})
-    if filename is None:
-        filename = ''
-    if len(neighbors) != 0 and hop_number < 5:
-        for neighbor in neighbors:
-
-            response = requests.get('http://{0}:{1}/api/v1/filelist?filename={2}&hop={3}'.format(neighbor.ip_address,
-                                                                                                 neighbor.port,
-                                                                                                 filename,
-                                                                                                 hop_number))
-            response = json.loads(response.text)
-            for response_file in response:
-                if filename in response_file['name']:
-                    json_response.append({'name': response_file['name'],
-                                          'location': response_file['location'],
-                                          'category': response_file['category'],
-                                          'host': response_file['host'],
-                                          'port': response_file['port']})
-
-    return HttpResponse(json.dumps(json_response))
+    return render(request, 'index.html')
 
 
 def search_results(request):
@@ -100,6 +56,9 @@ def search_neighbor(request):
     return response
 
 
+# ---------------API SECTION-------------------------
+
+
 def download_file(request):
     filename = request.GET.get('filename')
     try:
@@ -116,3 +75,41 @@ def download_file(request):
 
     logging.debug("Response: {0}".format(response))
     return response
+
+
+def filelist_api(request):
+    host, port = request.META['HTTP_HOST'].split(':')
+    neighbors = Neighbors.objects.all()
+    filename = request.GET.get('filename')
+    hop_number = int(request.GET.get('hop'))
+    hop_number += 1
+    filelist = File.objects.all()
+    json_response = []
+    for file in filelist:
+        json_response.append({'name': file.name,
+                              'location': file.location,
+                              'category': file.category,
+                              'parent': 'None',
+                              'host': host,
+                              'port': port})
+    if filename is None:
+        filename = ''
+    if len(neighbors) != 0 and hop_number < 5:
+        for neighbor in neighbors:
+
+            response = requests.get('http://{0}:{1}/api/v1/filelist?filename={2}&hop={3}'.format(neighbor.ip_address,
+                                                                                                 neighbor.port,
+                                                                                                 filename,
+                                                                                                 hop_number))
+            response = json.loads(response.text)
+            for response_file in response:
+                if filename in response_file['name']:
+                    json_response.append({'name': response_file['name'],
+                                          'location': response_file['location'],
+                                          'category': response_file['category'],
+                                          'parent': host,
+                                          'host': response_file['host'],
+                                          'port': response_file['port']})
+            print json_response
+
+    return HttpResponse(json.dumps(json_response))
