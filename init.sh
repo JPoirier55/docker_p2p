b26474169e0d77dbea909432535fdbffc5ac1e9e
jake <jake.poirier55@gmail.com>
2016-11-12 17:23:52 -0700

    added encryption scripts, new dockerfile

--- init.sh
+++ init.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-git clone https://github.com/jpoirier55/docker_p2p
-
-cd docker_p2p
-
 python manage.py makemigrations
 python manage.py migrate
 echo "Creating database user, requires input:"
